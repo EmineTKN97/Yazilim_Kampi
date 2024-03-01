7484f768510c07ad36e8f35f5fe56e11e49f6ac4
Emine TEKİN <eminetekin97@outlook.com>
2024-03-01 22:42:49 +0300

    +add handlerforbusiness
    +add handlerforbusiness

---
+++ nArchitecture(SeniorKamp)/src/demoProjects/rentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Brands.Commands.CreateBrand;
+using Application.Features.Brands.DTOs;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            //Mapleme profilleri yazılır
+            CreateMap<Brand, CreatedBrandDto>().ReverseMap();
+            CreateMap<Brand, CreateBrandCommand>().ReverseMap();//Tam tersi işlemlerde de kullanabilmek için reverse eklenir
+        }
+    }
+}
---
+++ nArchitecture(SeniorKamp)/src/demoProjects/rentACar/WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Brands.Commands.CreateBrand;
+using Application.Features.Brands.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+        [HttpPost]
+        //commandin maediator aracılığıyla handlesini bulması için görevlendiriyoruz
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+            CreatedBrandDto result = await Mediator.Send(createBrandCommand);
+            return Created("", result);
+        }
+    }
+}
